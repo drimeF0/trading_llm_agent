074d786b18a98100f86eac4eb5a6651b6a5ce701
drime <spogocev@gmail.com>
2025-11-01 19:03:00 +0300

    create dataset.py

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -9,11 +9,46 @@ import numpy as np
 class DataSource:
 
     def __init__(self, preprompt: str = ""):
-        pass
+        self.prepompt = preprompt
 
     
-    def at(self, datatime: datetime.datetime):
+    def at(self, datetime_: datetime.datetime) -> str:
         raise NotImplementedError
+
+
+class PandasDataSource(DataSource):
+
+    def __init__(self, df: pd.DataFrame, n: int = 1, *args, **kwargs):
+        self.df = df
+        self.n = n
+
+        super().__init__(*args, **kwargs)
+    
+    
+    def _at(self, datetime_: datetime.datetime, n: int = 1) -> pd.DataFrame:
+        df = self.df.loc[datetime_:]
+        df = df.head(n)
+        return df
+
+
+
+class RetailTGDataSource(PandasDataSource):
+
+    def at(self, datetime_: datetime.datetime) -> str:
+
+
+        df = self._at(datetime_, self.n)
+        
+
+        return_string = f"{self.prepompt}\n"
+
+        for username, message_text, message_date in zip(df["username"], df["message_text"], df["message_date"]):
+            msg_string = f"{message_date}{username}: {message_text}\n"
+            return_string += msg_string
+        
+        return return_string
+
+
 
 
 
@@ -28,5 +63,6 @@ class Dataset:
     def add_source(self, source: DataSource):
         self.sources.append(source)
 
-
+if __name__ == "__main__":
+    pass
 
